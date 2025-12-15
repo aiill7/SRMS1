5b21a9ab926991bc73adf5c03051d780924d0725
aiill7 <f4hyjfkx5s@privaterelay.appleid.com>
2025-12-16 00:54:41 +0300

    Create course.java

---
+++ src/course.java
@@ -0,0 +1,30 @@
+public class Course {
+private String courseCode;
+ 
+private String courseName;
+private int creditHours;
+
+    public Course(String courseCode, String courseName, int creditHours) {
+            this.courseCode = courseCode;
+        this.courseName = courseName;
+    
+   this.creditHours = creditHours;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public int getCreditHours() {
+        return creditHours;
+    }
+
+    @Override
+    public String toString() {
+        return courseCode + " - " + courseName + " (" + creditHours + " CH)";
+    }
+}
